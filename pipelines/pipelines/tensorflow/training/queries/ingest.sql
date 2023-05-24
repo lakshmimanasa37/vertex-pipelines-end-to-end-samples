0504422c78fc524327474ff9440cadc6d0af19dd
lakshmimanasa37 <35919173+lakshmimanasa37@users.noreply.github.com>
2023-05-24 11:43:20 +0100

    Update ingest.sql

--- pipelines/pipelines/tensorflow/training/queries/ingest.sql
+++ pipelines/pipelines/tensorflow/training/queries/ingest.sql
@@ -36,6 +36,6 @@ SELECT
     starttime,
     IF(EXTRACT(DAYOFWEEK FROM starttime) = 1 OR EXTRACT(DAYOFWEEK FROM starttime) = 7, true, false) AS is_weekend,
     tripduration AS `{{ target_column }}`
-FROM `bigquery-public-data.new_york_citibike.citibike_trips`
+FROM filtered_data
 WHERE
     (tripduration IS NOT NULL OR tripduration > 0)
