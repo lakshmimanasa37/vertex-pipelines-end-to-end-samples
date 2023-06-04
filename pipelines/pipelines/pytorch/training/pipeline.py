805ec6b9f6aab50a6b80f6b9b865e6291bff2e27
lakshmimanasa37 <35919173+lakshmimanasa37@users.noreply.github.com>
2023-06-04 08:52:19 +0100

    Update pipeline.py

--- pipelines/pipelines/pytorch/training/pipeline.py
+++ pipelines/pipelines/pytorch/training/pipeline.py
@@ -1,290 +1,262 @@
-import argparse
-import os
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
-import logging
-import sys
+import os
+import pathlib
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from pathlib import Path
-from torchvision.transforms import Normalize
-
-# used for monitoring during prediction time
-TRAINING_DATASET_INFO = "training_dataset.json"
-# numeric/categorical features in Chicago trips dataset to be preprocessed
-NUM_COLS = ["start_station_id", "end_station_id"]
-OHE_COLS = ["is_weekend"]
-
-DEFAULT_HPARAMS = dict(
-    batch_size=100,
-    epochs=1,
-    loss_fn="MSELoss",
-    optimizer="Adam",
-    learning_rate=0.001,
-    metrics=[
-        "RMSELoss",
-        "L1Loss",
-        "L1Loss",
-        "MSELoss",
-    ],
-    hidden_units=[(10, nn.ReLU())],
-    distribute_strategy="single",
-    early_stopping_epochs=5,
-    label="tripduration",
+from kfp.v2 import compiler, dsl
+from pipelines import generate_query
+from pipelines.components import (
+    extract_bq_to_dataset,
+    bq_query_to_table,
+    update_best_model,
+    import_model_evaluation,
+    custom_train_job,
+    lookup_model,
 )
 
-logging.getLogger().setLevel(logging.INFO)
 
-
-class CustomDataset(Dataset):
-    def __init__(self, input_data, label_name):
-        self.input_data = input_data
-        self.label_name = label_name
-
-    def __len__(self):
-        return len(self.input_data)
-
-    def __getitem__(self, idx):
-        return self.input_data[idx]
-
-    def get_labels(self):
-        return self.input_data[self.label_name].values
-
-
-def create_dataset(input_data, label_name, model_params):
-    """Create a PyTorch Dataset from input csv files.
-    Args:
-        input_data (pd.DataFrame): Train/Valid data in DataFrame format
-        label_name (str): Name of column containing the labels
-        model_params (dict): model parameters
-    Returns:
-        dataset (CustomDataset): PyTorch dataset
+@dsl.pipeline(name="tensorflow-train-pipeline")
+def tensorflow_pipeline(
+    project_id: str = os.environ.get("VERTEX_PROJECT_ID"),
+    project_location: str = os.environ.get("VERTEX_LOCATION"),
+    ingestion_project_id: str = os.environ.get("VERTEX_PROJECT_ID"),
+    model_name: str = "simple_tensorflow",
+    dataset_id: str = "preprocessing",
+    dataset_location: str = os.environ.get("VERTEX_LOCATION"),
+    ingestion_dataset_id: str = "new_york_citibike",
+    timestamp: str = "2016-12-01 00:00:00",
+    staging_bucket: str = os.environ.get("VERTEX_PIPELINE_ROOT"),
+    pipeline_files_gcs_path: str = os.environ.get("PIPELINE_FILES_GCS_PATH"),
+    test_dataset_uri: str = "",
+):
     """
-    logging.info(f"Creating dataset from DataFrame...")
-    dataset = CustomDataset(input_data, label_name)
-    return dataset
+    Tensorflow Keras training pipeline which:
+     1. Splits and extracts a dataset from BQ to GCS
+     2. Trains a model via Vertex AI CustomTrainingJob
+     3. Evaluates the model against the current champion model
+     4. If better the model becomes the new default model
 
-
-def get_distribution_strategy(distribute_strategy):
-    """Set distribute strategy based on input string.
     Args:
-        distribute_strategy (str): single, mirror or multi
-    Returns:
-        strategy (None): No distribution strategy implemented in PyTorch
+        project_id (str): project id of the Google Cloud project
+        project_location (str): location of the Google Cloud project
+        pipeline_files_gcs_path (str): GCS path where the pipeline files are located
+        ingestion_project_id (str): project id containing the source bigquery data
+            for ingestion. This can be the same as `project_id` if the source data is
+            in the same project where the ML pipeline is executed.
+        model_name (str): name of model
+        model_label (str): label of model
+        dataset_id (str): id of BQ dataset used to store all staging data & predictions
+        dataset_location (str): location of dataset
+        ingestion_dataset_id (str): dataset id of ingestion data
+        timestamp (str): Optional. Empty or a specific timestamp in ISO 8601 format
+            (YYYY-MM-DDThh:mm:ss.sss±hh:mm or YYYY-MM-DDThh:mm:ss).
+            If any time part is missing, it will be regarded as zero.
+        staging_bucket (str): Staging bucket for pipeline artifacts.
+        pipeline_files_gcs_path (str): GCS path where the pipeline files are located
+        test_dataset_uri (str): Optional. GCS URI of statis held-out test dataset.
     """
-    logging.info(f"Distribution strategy: {distribute_strategy}")
 
-    if distribute_strategy == "single":
-        strategy = None
-    elif distribute_strategy == "mirror" or distribute_strategy == "multi":
-        raise RuntimeError(f"Distribute strategy: {distribute_strategy} not supported")
-    else:
-        raise RuntimeError(f"Distribute strategy: {distribute_strategy} not supported")
-    return strategy
+    # Create variables to ensure the same arguments are passed
+    # into different components of the pipeline
+    label_column_name = "tripduration"
+    time_column = "starttime"
+    ingestion_table = "citibike_trips"
+    table_suffix = "_tf_training"  # suffix to table names
+    ingested_table = "ingested_data" + table_suffix
+    preprocessed_table = "preprocessed_data" + table_suffix
+    train_table = "train_data" + table_suffix
+    valid_table = "valid_data" + table_suffix
+    test_table = "test_data" + table_suffix
+    primary_metric = "rootMeanSquaredError"
+    train_script_uri = f"{pipeline_files_gcs_path}/training/assets/train_tf_model.py"
+    hparams = dict(
+        batch_size=100,
+        epochs=5,
+        loss_fn="MeanSquaredError",
+        optimizer="Adam",
+        learning_rate=0.01,
+        hidden_units=[(64, "relu"), (32, "relu")],
+        distribute_strategy="single",
+        early_stopping_epochs=5,
+    )
+
+    # generate sql queries which are used in ingestion and preprocessing
+    # operations
+
+    queries_folder = pathlib.Path(__file__).parent / "queries"
+
+    ingest_query = generate_query(
+        queries_folder / "ingest.sql",
+        source_dataset=f"{ingestion_project_id}.{ingestion_dataset_id}",
+        source_table=ingestion_table,
+        filter_column=time_column,
+        target_column=label_column_name,
+        filter_start_value=timestamp,
+    )
+    split_train_query = generate_query(
+        queries_folder / "sample.sql",
+        source_dataset=dataset_id,
+        source_table=ingested_table,
+        num_lots=10,
+        lots=tuple(range(8)),
+    )
+    split_valid_query = generate_query(
+        queries_folder / "sample.sql",
+        source_dataset=dataset_id,
+        source_table=ingested_table,
+        num_lots=10,
+        lots="(8)",
+    )
+    split_test_query = generate_query(
+        queries_folder / "sample.sql",
+        source_dataset=dataset_id,
+        source_table=ingested_table,
+        num_lots=10,
+        lots="(9)",
+    )
+    data_cleaning_query = generate_query(
+        queries_folder / "engineer_features.sql",
+        source_dataset=dataset_id,
+        source_table=train_table,
+    )
+
+    # data ingestion and preprocessing operations
+
+    kwargs = dict(
+        bq_client_project_id=project_id,
+        destination_project_id=project_id,
+        dataset_id=dataset_id,
+        dataset_location=dataset_location,
+        query_job_config=json.dumps(dict(write_disposition="WRITE_TRUNCATE")),
+    )
+    ingest = bq_query_to_table(
+        query=ingest_query, table_id=ingested_table, **kwargs
+    ).set_display_name("Ingest data")
+
+    # exporting data to GCS from BQ
+    split_train_data = (
+        bq_query_to_table(query=split_train_query, table_id=train_table, **kwargs)
+        .after(ingest)
+        .set_display_name("Split train data")
+    )
+    split_valid_data = (
+        bq_query_to_table(query=split_valid_query, table_id=valid_table, **kwargs)
+        .after(ingest)
+        .set_display_name("Split validation data")
+    )
+    split_test_data = (
+        bq_query_to_table(query=split_test_query, table_id=test_table, **kwargs)
+        .after(ingest)
+        .set_display_name("Split test data")
+    )
+    data_cleaning = (
+        bq_query_to_table(
+            query=data_cleaning_query, table_id=preprocessed_table, **kwargs
+        )
+        .after(split_train_data)
+        .set_display_name("Clean data")
+    )
+
+    # data extraction to gcs
+
+    train_dataset = (
+        extract_bq_to_dataset(
+            bq_client_project_id=project_id,
+            source_project_id=project_id,
+            dataset_id=dataset_id,
+            table_name=preprocessed_table,
+            dataset_location=dataset_location,
+        )
+        .after(data_cleaning)
+        .set_display_name("Extract train data to storage")
+    ).outputs["dataset"]
+    valid_dataset = (
+        extract_bq_to_dataset(
+            bq_client_project_id=project_id,
+            source_project_id=project_id,
+            dataset_id=dataset_id,
+            table_name=valid_table,
+            dataset_location=dataset_location,
+        )
+        .after(split_valid_data)
+        .set_display_name("Extract validation data to storage")
+    ).outputs["dataset"]
+    test_dataset = (
+        extract_bq_to_dataset(
+            bq_client_project_id=project_id,
+            source_project_id=project_id,
+            dataset_id=dataset_id,
+            table_name=test_table,
+            dataset_location=dataset_location,
+            destination_gcs_uri=test_dataset_uri,
+        )
+        .after(split_test_data)
+        .set_display_name("Extract test data to storage")
+        .set_caching_options(False)
+    ).outputs["dataset"]
+
+    existing_model = (
+        lookup_model(
+            model_name=model_name,
+            project_location=project_location,
+            project_id=project_id,
+            fail_on_model_not_found=False,
+        )
+        .set_display_name("Lookup past model")
+        .outputs["model_resource_name"]
+    )
+
+    train_model = custom_train_job(
+        train_script_uri=train_script_uri,
+        train_data=train_dataset,
+        valid_data=valid_dataset,
+        test_data=test_dataset,
+        project_id=project_id,
+        project_location=project_location,
+        model_display_name=model_name,
+        train_container_uri="europe-docker.pkg.dev/vertex-ai/training/tf-cpu.2-6:latest",  # noqa: E501
+        serving_container_uri="europe-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-6:latest",  # noqa: E501
+        hparams=hparams,
+        staging_bucket=staging_bucket,
+        parent_model=existing_model,
+    ).set_display_name("Train model")
+
+    evaluation = import_model_evaluation(
+        model=train_model.outputs["model"],
+        metrics=train_model.outputs["metrics"],
+        test_dataset=test_dataset,
+        pipeline_job_id="{{$.pipeline_job_name}}",
+        project_location=project_location,
+    ).set_display_name("Import evaluation")
+
+    with dsl.Condition(existing_model != "", "champion-exists"):
+        update_best_model(
+            challenger=train_model.outputs["model"],
+            challenger_evaluation=evaluation.outputs["model_evaluation"],
+            parent_model=existing_model,
+            eval_metric=primary_metric,
+            eval_lower_is_better=True,
+            project_id=project_id,
+            project_location=project_location,
+        ).set_display_name("Update best model")
 
 
-def normalization(name, dataset):
-    """Create a normalization layer for a feature.
-    Args:
-        name (str): name of feature to be normalized
-        dataset (CustomDataset): dataset to adapt layer
-    Returns:
-        normalization layer (Normalize): adapted normalization layer
-    """
-    logging.info(f"Normalizing numerical input '{name}'...")
-    x = Normalize(dataset.input_data[name].mean(), dataset.input_data[name].std())
-    return x
-
-
-def build_and_compile_model(dataset, model_params):
-    """Build and compile model.
-    Args:
-        dataset (CustomDataset): training dataset
-        model_params (dict): model parameters
-    Returns:
-        model (nn.Module): built and compiled model
-    """
-    input_size = len(NUM_COLS) + len(OHE_COLS)
-    hidden_units = model_params["hidden_units"]
-
-    class Model(nn.Module):
-        def __init__(self):
-            super(Model, self).__init__()
-            self.normalizations = nn.ModuleList([normalization(name, dataset) for name in NUM_COLS])
-            self.embeddings = nn.ModuleList()
-
-            for name in OHE_COLS:
-                num_classes = len(dataset.input_data[name].unique())
-                embedding = nn.Embedding(num_classes, hidden_units[0][0])
-                self.embeddings.append(embedding)
-
-            self.linear1 = nn.Linear(input_size * hidden_units[0][0], hidden_units[0][0])
-            self.relu1 = hidden_units[0][1]
-            self.linear2 = nn.Linear(hidden_units[0][0], 1)
-
-        def forward(self, x):
-            num_inputs = [self.normalizations[i](x[:, i]) for i in range(len(NUM_COLS))]
-            ohe_inputs = [self.embeddings[i](x[:, len(NUM_COLS) + i].long()) for i in range(len(OHE_COLS))]
-
-            num_inputs = torch.cat(num_inputs, dim=1)
-            ohe_inputs = torch.cat(ohe_inputs, dim=1)
-
-            x = torch.cat((num_inputs, ohe_inputs), dim=1)
-            x = self.linear1(x)
-            x = self.relu1(x)
-            x = self.linear2(x)
-
-            return x
-
-    model = Model()
-    logging.info(f"Model:\n{model}")
-    logging.info(f"Use optimizer {model_params['optimizer']}")
-    optimizer = getattr(optim, model_params["optimizer"])
-    optimizer = optimizer(model.parameters(), lr=model_params["learning_rate"])
-
-    criterion = getattr(nn, model_params["loss_fn"])
-    metrics = model_params["metrics"]
-
-    return model, optimizer, criterion, metrics
-
-
-def _is_chief(strategy):
-    """Determine whether current worker is the chief (master).
-    Args:
-        strategy (None): No distributed training in PyTorch
-    Returns:
-        is_chief (bool): True if worker is chief, otherwise False
-    """
-    return True
-
-
-def train(model, optimizer, criterion, dataloader, metrics):
-    """Train the model.
-    Args:
-        model (nn.Module): PyTorch model
-        optimizer (optim.Optimizer): PyTorch optimizer
-        criterion (nn.Module): PyTorch loss function
-        dataloader (DataLoader): PyTorch data loader
-        metrics (list): List of metrics to evaluate
-    Returns:
-        history (dict): Training history
-    """
-    model.train()
-    history = {}
-
-    for epoch in range(hparams["epochs"]):
-        running_loss = 0.0
-
-        for batch in dataloader:
-            inputs = batch.float()
-            labels = inputs[:, -1]  # Assuming label is the last column
-
-            optimizer.zero_grad()
-
-            outputs = model(inputs)
-            loss = criterion(outputs.squeeze(), labels)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-        epoch_loss = running_loss / len(dataloader)
-        logging.info(f"Epoch [{epoch+1}/{hparams['epochs']}], Loss: {epoch_loss:.4f}")
-
-        history["loss"] = history.get("loss", []) + [epoch_loss]
-
-    return history
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--train_data", type=str, required=True)
-parser.add_argument("--valid_data", type=str, required=True)
-parser.add_argument("--test_data", type=str, required=True)
-parser.add_argument("--model", default=os.getenv("AIP_MODEL_DIR"), type=str, help="")
-parser.add_argument("--metrics", type=str, required=True)
-parser.add_argument("--hparams", default={}, type=json.loads)
-args = parser.parse_args()
-
-hparams = {**DEFAULT_HPARAMS, **args.hparams}
-logging.info(f"Using model hyper-parameters: {hparams}")
-label = hparams["label"]
-
-strategy = get_distribution_strategy(hparams["distribute_strategy"])
-
-train_data = pd.read_csv(args.train_data)
-valid_data = pd.read_csv(args.valid_data)
-test_data = pd.read_csv(args.test_data)
-
-train_dataset = create_dataset(train_data, label, hparams)
-valid_dataset = create_dataset(valid_data, label, hparams)
-test_dataset = create_dataset(test_data, label, hparams)
-
-train_dataloader = DataLoader(train_dataset, batch_size=hparams["batch_size"], shuffle=True)
-valid_dataloader = DataLoader(valid_dataset, batch_size=hparams["batch_size"], shuffle=False)
-test_dataloader = DataLoader(test_dataset, batch_size=hparams["batch_size"], shuffle=False)
-
-train_features = train_data.columns.tolist()
-valid_features = valid_data.columns.tolist()
-logging.info(f"Training feature names: {train_features}")
-logging.info(f"Validation feature names: {valid_features}")
-
-if len(train_features) != len(valid_features):
-    raise RuntimeError(f"No. of training features != # validation features")
-
-torch_model, optimizer, criterion, metrics = build_and_compile_model(train_dataset, hparams)
-
-logging.info("Fit model...")
-history = train(torch_model, optimizer, criterion, train_dataloader, metrics)
-
-# only persist output files if current worker is chief
-if not _is_chief(strategy):
-    logging.info("not chief node, exiting now")
-    sys.exit()
-
-os.makedirs(args.model, exist_ok=True)
-logging.info(f"Save model to: {args.model}")
-torch.save(torch_model.state_dict(), os.path.join(args.model, "model.pt"))
-
-logging.info(f"Save metrics to: {args.metrics}")
-eval_metrics = {}
-
-with torch.no_grad():
-    for batch in test_dataloader:
-        inputs = batch.float()
-        labels = inputs[:, -1]  # Assuming label is the last column
-
-        outputs = torch_model(inputs)
-        loss = criterion(outputs.squeeze(), labels)
-
-        for metric in metrics:
-            if metric == "RMSELoss":
-                eval_metrics[metric] = torch.sqrt(loss).item()
-            else:
-                eval_metrics[metric] = loss.item()
-
-metrics = {
-    "problemType": "regression",
-    "rootMeanSquaredError": eval_metrics["RMSELoss"],
-    "meanAbsoluteError": eval_metrics["L1Loss"],
-    "meanAbsolutePercentageError": eval_metrics["L1Loss"],
-    "rSquared": None,
-    "rootMeanSquaredLogError": eval_metrics["MSELoss"],
-}
-
-with open(args.metrics, "w") as fp:
-    json.dump(metrics, fp)
-
-# Persist URIs of training file(s) for model monitoring in batch predictions
-path = Path(args.model) / TRAINING_DATASET_INFO
-training_dataset_for_monitoring = {
-    "gcsSource": {"uris": [args.train_data]},
-    "dataFormat": "csv",
-    "targetField": hparams["label"],
-}
-logging.info(f"Save training dataset info for model monitoring: {path}")
-logging.info(f"Training dataset: {training_dataset_for_monitoring}")
-
-with open(path, "w") as fp:
-    json.dump(training_dataset_for_monitoring, fp)
+if __name__ == "__main__":
+    compiler.Compiler().compile(
+        pipeline_func=tensorflow_pipeline,
+        package_path="training.json",
+        type_check=False,
+    )
